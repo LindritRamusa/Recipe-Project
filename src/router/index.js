a4e246bbcb975e77e52e0bd9d8a5efc06b4761ec
LindritRamusa <82161409+LindritRamusa@users.noreply.github.com>
2022-05-03 23:23:59 +0200

    Commit 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/HomeView.vue'
+// import { createRouter, createWebHistory } from 'vue-router'
+// import Home from '../views/HomeView.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'HomeView',
-    component: Home
-  },
-  {
-    path: '/recipe/:slug',
-    name: 'RecipeView',
-    component: () => import('../views/RecipeView.vue')
-  }
-]
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'HomeView',
+//     component: Home
+//   },
+//   {
+//     path: '/recipe/:slug',
+//     name: 'RecipeView',
+//     component: () => import('../views/RecipeView.vue')
+//   }
+// ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+// const router = createRouter({
+//   history: createWebHistory(process.env.BASE_URL),
+//   routes
+// })
 
-export default router
+// export default router
